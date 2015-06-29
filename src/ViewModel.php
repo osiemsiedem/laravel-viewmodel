e25c535599f3613318e55f41ae296a2fc7b58a26
Marek Szymczuk <marek.szymczuk@gmail.com>
2015-06-29 21:04:17 +0200

    Convert to PSR-2.

--- src/ViewModel.php
+++ src/ViewModel.php
@@ -8,280 +8,267 @@ use ReflectionProperty;
 use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Contracts\Support\Arrayable;
 
-abstract class ViewModel implements ArrayAccess, Arrayable, Jsonable, JsonSerializable {
+abstract class ViewModel implements ArrayAccess, Arrayable, Jsonable, JsonSerializable
+{
+    /**
+     * Create a new ViewModel instance.
+     *
+     * @param  array  $data
+     */
+    public function __construct(array $data = [])
+    {
+        $this->set($data);
+    }
 
-	/**
-	 * Create a new ViewModel instance.
-	 *
-	 * @param  array  $data
-	 */
-	public function __construct(array $data = [])
-	{
-		$this->set($data);
-	}
+    /**
+     * Convert the model instance to an array.
+     *
+     * @return  array
+     */
+    public function toArray()
+    {
+        $reflection = new ReflectionObject($this);
 
-	/**
-	 * Convert the model instance to an array.
-	 *
-	 * @return  array
-	 */
-	public function toArray()
-	{
-		$reflection = new ReflectionObject($this);
+        $properties = $this->propertiesToArray($reflection);
+        $methods    = $this->methodsToArray($reflection);
 
-		$properties = $this->propertiesToArray($reflection);
-		$methods    = $this->methodsToArray($reflection);
+        return array_merge($properties, $methods);
+    }
 
-		return array_merge($properties, $methods);
-	}
+    /**
+     * Convert all the public properties to an array.
+     *
+     * @param   ReflectionObject  $reflection
+     * @return  array
+     */
+    protected function propertiesToArray(ReflectionObject $reflection)
+    {
+        $properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
 
-	/**
-	 * Convert all the public properties to an array.
-	 *
-	 * @param   ReflectionObject  $reflection
-	 * @return  array
-	 */
-	protected function propertiesToArray(ReflectionObject $reflection)
-	{
-		$properties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
+        $data = [];
 
-		$data = [];
+        foreach ($properties as $property) {
+            if ($property->class !== __CLASS__) {
+                $data[$property->name] = $this->{$property->name};
+            }
+        }
 
-		foreach ($properties as $property)
-		{
-			if ($property->class !== __CLASS__)
-			{
-				$data[$property->name] = $this->{$property->name};
-			}
-		}
+        return $data;
+    }
 
-		return $data;
-	}
+    /**
+     * Convert all the public methods to an array.
+     *
+     * @param   ReflectionObject  $reflection
+     * @return  array
+     */
+    protected function methodsToArray(ReflectionObject $reflection)
+    {
+        $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
 
-	/**
-	 * Convert all the public methods to an array.
-	 *
-	 * @param   ReflectionObject  $reflection
-	 * @return  array
-	 */
-	protected function methodsToArray(ReflectionObject $reflection)
-	{
-		$methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+        $data = [];
 
-		$data = [];
+        foreach ($methods as $method) {
+            if ($method->class !== __CLASS__ && substr($method->name, 0, 2) !== '__') {
+                $data[$method->name] = $this->{$method->name}();
+            }
+        }
 
-		foreach ($methods as $method)
-		{
-			if ($method->class !== __CLASS__ && substr($method->name, 0, 2) !== '__')
-			{
-				$data[$method->name] = $this->{$method->name}();
-			}
-		}
+        return $data;
+    }
 
-		return $data;
-	}
+    /**
+     * Convert the model instance to JSON.
+     *
+     * @param   int  $options
+     * @return  string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->toArray(), $options);
+    }
 
-	/**
-	 * Convert the model instance to JSON.
-	 *
-	 * @param   int  $options
-	 * @return  string
-	 */
-	public function toJson($options = 0)
-	{
-		return json_encode($this->toArray(), $options);
-	}
+    /**
+     * Convert the model into something JSON serializable.
+     *
+     * @return  array
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
 
-	/**
-	 * Convert the model into something JSON serializable.
-	 *
-	 * @return  array
-	 */
-	public function jsonSerialize()
-	{
-		return $this->toArray();
-	}
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param   string  $key
+     * @return  bool
+     */
+    public function has($key)
+    {
+        return ($this->hasAccessibleMethod($key) || $this->hasAccessibleProperty($key));
+    }
 
-	/**
-	 * Determine if the given attribute exists.
-	 *
-	 * @param   string  $key
-	 * @return  bool
-	 */
-	public function has($key)
-	{
-		return ($this->hasAccessibleMethod($key) || $this->hasAccessibleProperty($key));
-	}
+    /**
+     * Determine if the given method is public.
+     *
+     * @param   string  $key
+     * @return  bool
+     */
+    protected function hasAccessibleMethod($key)
+    {
+        if (method_exists($this, $key)) {
+            $reflection = new ReflectionMethod($this, $key);
 
-	/**
-	 * Determine if the given method is public.
-	 *
-	 * @param   string  $key
-	 * @return  bool
-	 */
-	protected function hasAccessibleMethod($key)
-	{
-		if (method_exists($this, $key))
-		{
-			$reflection = new ReflectionMethod($this, $key);
+            return $reflection->isPublic();
+        }
 
-			return $reflection->isPublic();
-		}
+        return false;
+    }
 
-		return false;
-	}
+    /**
+     * Determine if the given property is public.
+     *
+     * @param   string  $key
+     * @return  bool
+     */
+    protected function hasAccessibleProperty($key)
+    {
+        if (isset($this->$key)) {
+            $reflection = new ReflectionProperty($this, $key);
 
-	/**
-	 * Determine if the given property is public.
-	 *
-	 * @param   string  $key
-	 * @return  bool
-	 */
-	protected function hasAccessibleProperty($key)
-	{
-		if (isset($this->$key))
-		{
-			$reflection = new ReflectionProperty($this, $key);
+            return $reflection->isPublic();
+        }
 
-			return $reflection->isPublic();
-		}
+        return false;
+    }
 
-		return false;
-	}
+    /**
+     * Get an attribute from the model.
+     *
+     * @param   string  $key
+     * @return  mixed
+     */
+    public function get($key, $default = null)
+    {
+        if ($this->hasAccessibleMethod($key)) {
+            return $this->$key();
+        }
 
-	/**
-	 * Get an attribute from the model.
-	 *
-	 * @param   string  $key
-	 * @return  mixed
-	 */
-	public function get($key, $default = null)
-	{
-		if ($this->hasAccessibleMethod($key))
-		{
-			return $this->$key();
-		}
+        if ($this->hasAccessibleProperty($key)) {
+            return $this->$key;
+        }
 
-		if ($this->hasAccessibleProperty($key))
-		{
-			return $this->$key;
-		}
+        return $default;
+    }
 
-		return $default;
-	}
+    /**
+     * Set a given attribute on the model.
+     *
+     * @param   mixed  $key
+     * @param   mixed  $value
+     * @return  $this
+     */
+    public function set($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $k => $v) {
+                $this->$k = $v;
+            }
+        } else {
+            $this->$key = $value;
+        }
 
-	/**
-	 * Set a given attribute on the model.
-	 *
-	 * @param   mixed  $key
-	 * @param   mixed  $value
-	 * @return  $this
-	 */
-	public function set($key, $value = null)
-	{
-		if (is_array($key))
-		{
-			foreach ($key as $k => $v)
-			{
-				$this->$k = $v;
-			}
-		}
-		else
-		{
-			$this->$key = $value;
-		}
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Remove an attribute from the model.
+     *
+     * @param   mixed  $key
+     * @return  $this
+     */
+    public function forget($key)
+    {
+        unset($this->$key);
 
-	/**
-	 * Remove an attribute from the model.
-	 *
-	 * @param   mixed  $key
-	 * @return  $this
-	 */
-	public function forget($key)
-	{
-		unset($this->$key);
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Determine if the given offset exists.
+     *
+     * @param   string  $offset
+     * @return  bool
+     */
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
 
-	/**
-	 * Determine if the given offset exists.
-	 *
-	 * @param   string  $offset
-	 * @return  bool
-	 */
-	public function offsetExists($offset)
-	{
-		return $this->has($offset);
-	}
+    /**
+     * Get the value for a given offset.
+     *
+     * @param   string  $offset
+     * @return  mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
 
-	/**
-	 * Get the value for a given offset.
-	 *
-	 * @param   string  $offset
-	 * @return  mixed
-	 */
-	public function offsetGet($offset)
-	{
-		return $this->get($offset);
-	}
+    /**
+     * Set the value at the given offset.
+     *
+     * @param   string  $offset
+     * @param   mixed   $value
+     * @return  void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->set($offset, $value);
+    }
 
-	/**
-	 * Set the value at the given offset.
-	 *
-	 * @param   string  $offset
-	 * @param   mixed   $value
-	 * @return  void
-	 */
-	public function offsetSet($offset, $value)
-	{
-		$this->set($offset, $value);
-	}
+    /**
+     * Unset the value at the given offset.
+     *
+     * @param   string  $offset
+     * @return  void
+     */
+    public function offsetUnset($offset)
+    {
+        $this->forget($offset);
+    }
 
-	/**
-	 * Unset the value at the given offset.
-	 *
-	 * @param   string  $offset
-	 * @return  void
-	 */
-	public function offsetUnset($offset)
-	{
-		$this->forget($offset);
-	}
+    /**
+     * Convert the model to its string representation.
+     *
+     * @return  string
+     */
+    public function __toString()
+    {
+        return $this->toJson();
+    }
 
-	/**
-	 * Convert the model to its string representation.
-	 *
-	 * @return  string
-	 */
-	public function __toString()
-	{
-		return $this->toJson();
-	}
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param   string  $key
+     * @return  void
+     */
+    public function __isset($key)
+    {
+        return $this->has($key);
+    }
 
-	/**
-	 * Determine if the given attribute exists.
-	 *
-	 * @param   string  $key
-	 * @return  void
-	 */
-	public function __isset($key)
-	{
-		return $this->has($key);
-	}
-
-	/**
-	 * Dynamically retrieve attributes on the model.
-	 *
-	 * @param   string  $key
-	 * @return  mixed
-	 */
-	public function __get($key)
-	{
-		return $this->get($key);
-	}
-
+    /**
+     * Dynamically retrieve attributes on the model.
+     *
+     * @param   string  $key
+     * @return  mixed
+     */
+    public function __get($key)
+    {
+        return $this->get($key);
+    }
 }
--- tests/ViewModelTest.php
+++ tests/ViewModelTest.php
@@ -2,104 +2,102 @@
 
 use OsiemSiedem\View\ViewModel;
 
-class ViewModelTest extends PHPUnit_Framework_TestCase {
+class ViewModelTest extends PHPUnit_Framework_TestCase
+{
+    public function testViewModelGettersSetters()
+    {
+        $viewmodel = $this->getViewModel();
 
-	public function testViewModelGettersSetters()
-	{
-		$viewmodel = $this->getViewModel();
+        // toArray()
+        $this->assertInstanceOf('Illuminate\Contracts\Support\Arrayable', $viewmodel);
+        $this->assertEquals($viewmodel->toArray(), ['foo' => 'bar', 'bar' => 'foo']);
 
-		// toArray()
-		$this->assertInstanceOf('Illuminate\Contracts\Support\Arrayable', $viewmodel);
-		$this->assertEquals($viewmodel->toArray(), ['foo' => 'bar', 'bar' => 'foo']);
+        // toJson()
+        $this->assertInstanceOf('Illuminate\Contracts\Support\Jsonable', $viewmodel);
+        $this->assertEquals($viewmodel->toJson(), '{"foo":"bar","bar":"foo"}');
 
-		// toJson()
-		$this->assertInstanceOf('Illuminate\Contracts\Support\Jsonable', $viewmodel);
-		$this->assertEquals($viewmodel->toJson(), '{"foo":"bar","bar":"foo"}');
+        // jsonSerialize()
+        $this->assertInstanceOf('JsonSerializable', $viewmodel);
+        $this->assertEquals($viewmodel->jsonSerialize(), ['foo' => 'bar', 'bar' => 'foo']);
 
-		// jsonSerialize()
-		$this->assertInstanceOf('JsonSerializable', $viewmodel);
-		$this->assertEquals($viewmodel->jsonSerialize(), ['foo' => 'bar', 'bar' => 'foo']);
+        // has()
+        $this->assertTrue($viewmodel->has('foo'));
+        $this->assertFalse($viewmodel->has('xyz'));
+        $this->assertFalse($viewmodel->has('hidden'));
 
-		// has()
-		$this->assertTrue($viewmodel->has('foo'));
-		$this->assertFalse($viewmodel->has('xyz'));
-		$this->assertFalse($viewmodel->has('hidden'));
+        // get()
+        $this->assertEquals($viewmodel->get('foo'), 'bar');
+        $this->assertEquals($viewmodel->get('bar'), 'foo');
+        $this->assertEquals($viewmodel->get('hidden'), null);
 
-		// get()
-		$this->assertEquals($viewmodel->get('foo'), 'bar');
-		$this->assertEquals($viewmodel->get('bar'), 'foo');
-		$this->assertEquals($viewmodel->get('hidden'), null);
+        // set()
+        $viewmodel->set('xyz', '123');
+        $this->assertTrue($viewmodel->has('xyz'));
+        $this->assertEquals($viewmodel->get('xyz'), '123');
 
-		// set()
-		$viewmodel->set('xyz', '123');
-		$this->assertTrue($viewmodel->has('xyz'));
-		$this->assertEquals($viewmodel->get('xyz'), '123');
+        $viewmodel->set(['a' => '1', 'b' => '2']);
+        $this->assertEquals($viewmodel->get('a'), '1');
+        $this->assertEquals($viewmodel->get('b'), '2');
 
-		$viewmodel->set(['a' => '1', 'b' => '2']);
-		$this->assertEquals($viewmodel->get('a'), '1');
-		$this->assertEquals($viewmodel->get('b'), '2');
+        // forget()
+        $viewmodel->forget('xyz');
+        $this->assertFalse($viewmodel->has('xyz'));
+        $this->assertEquals($viewmodel->get('xyz'), null);
+    }
 
-		// forget()
-		$viewmodel->forget('xyz');
-		$this->assertFalse($viewmodel->has('xyz'));
-		$this->assertEquals($viewmodel->get('xyz'), null);
-	}
+    public function testViewModelArrayAccess()
+    {
+        $viewmodel = $this->getViewModel();
 
-	public function testViewModelArrayAccess()
-	{
-		$viewmodel = $this->getViewModel();
+        $this->assertInstanceOf('ArrayAccess', $viewmodel);
 
-		$this->assertInstanceOf('ArrayAccess', $viewmodel);
+        // offsetExists() / offsetGet()
+        $this->assertTrue($viewmodel->offsetExists('foo'));
+        $this->assertEquals($viewmodel->offsetGet('foo'), 'bar');
 
-		// offsetExists() / offsetGet()
-		$this->assertTrue($viewmodel->offsetExists('foo'));
-		$this->assertEquals($viewmodel->offsetGet('foo'), 'bar');
+        // offsetSet() / offsetGet()
+        $viewmodel->offsetSet('foo', 'baz');
+        $this->assertEquals($viewmodel->offsetGet('foo'), 'baz');
 
-		// offsetSet() / offsetGet()
-		$viewmodel->offsetSet('foo', 'baz');
-		$this->assertEquals($viewmodel->offsetGet('foo'), 'baz');
+        // offsetUnset()
+        $viewmodel->offsetUnset('foo');
+        $this->assertFalse($viewmodel->offsetExists('foo'));
+    }
 
-		// offsetUnset()
-		$viewmodel->offsetUnset('foo');
-		$this->assertFalse($viewmodel->offsetExists('foo'));
-	}
+    public function testViewModelMagicMethods()
+    {
+        $viewmodel = $this->getViewModel();
 
-	public function testViewModelMagicMethods()
-	{
-		$viewmodel = $this->getViewModel();
+        // __isset() / __get()
+        $this->assertTrue(isset($viewmodel->foo));
+        $this->assertEquals($viewmodel->foo, 'bar');
 
-		// __isset() / __get()
-		$this->assertTrue(isset($viewmodel->foo));
-		$this->assertEquals($viewmodel->foo, 'bar');
+        $this->assertTrue(isset($viewmodel->bar));
+        $this->assertEquals($viewmodel->bar, 'foo');
 
-		$this->assertTrue(isset($viewmodel->bar));
-		$this->assertEquals($viewmodel->bar, 'foo');
+        // __toString()
+        $this->assertEquals((string) $viewmodel, '{"foo":"bar","bar":"foo"}');
 
-		// __toString()
-		$this->assertEquals( (string) $viewmodel, '{"foo":"bar","bar":"foo"}');
+        // __construct()
+        $viewmodel = $this->getViewModel(['a' => 'b']);
 
-		// __construct()
-		$viewmodel = $this->getViewModel(['a' => 'b']);
+        $this->assertEquals($viewmodel->a, 'b');
+    }
 
-		$this->assertEquals($viewmodel->a, 'b');
-	}
-
-	protected function getViewModel(array $data = [])
-	{
-		return new TestViewModel($data);
-	}
-
+    protected function getViewModel(array $data = [])
+    {
+        return new TestViewModel($data);
+    }
 }
 
-class TestViewModel extends ViewModel {
+class TestViewModel extends ViewModel
+{
+    public $foo = 'bar';
 
-	public $foo = 'bar';
+    protected $hidden = 'qwerty';
 
-	protected $hidden = 'qwerty';
-
-	public function bar()
-	{
-		return 'foo';
-	}
-
+    public function bar()
+    {
+        return 'foo';
+    }
 }
